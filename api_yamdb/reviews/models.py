8c95ea153f85bd9fecfe55dd710260c5eb838682
Кристина Стадникова <ururusenka32@yandex.ru>
2023-04-11 21:24:13 +0300

    Модели произведений, csv файлы

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+    title = models.ForeignKey(Title, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return f'{self.genre} {self.title}'
